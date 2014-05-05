34152d43fdf8cc53416a15d4adde769a82eab08e
justinobney <justin@envoc.com>
2014-05-05 11:06:32 -0500

    adding the template provider

---
+++ src/app/oTemplateProvider.js
@@ -0,0 +1,30 @@
+﻿(function() {
+    'use strict';
+
+    function TemplateUtils($templateCache, $http) {
+        this.setTemplate = function (path, html) {
+            $templateCache.put(path, html);
+        };
+
+        this.mapTemplateToUrl = function (path, url) {
+            $http
+                .get(url)
+                .then(function (response) {
+                    $templateCache.put(path, response.data);
+                });
+        };
+    }
+
+    angular
+        .module('envoc.directives')
+        .provider('oTemplate', function() {
+            this.$get = ['$injector', function ($injector) {
+                var $templateCache, $http;
+
+                $templateCache = $injector.get('$templateCache');
+                $http = $injector.get('$http');
+
+                return new TemplateUtils($templateCache, $http);
+            }];
+        });
+})();
