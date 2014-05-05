781712d9e6822fe67cf13094fba8d1cc19478be4
justinobney <justin@envoc.com>
2014-05-05 11:06:49 -0500

    changing example app to show setting a custom template

--- example/app.js
+++ example/app.js
@@ -1,19 +1,30 @@
 ﻿var app = angular.module('plunker', ['envoc.directives']);
 
-app.controller('MainCtrl', function ($timeout) {
+// comment me out to see default template
+app.run([
+    'oTemplate', function (oTemplate) {
+        oTemplate.setTemplate('/oTemplates/oValidationMessageFor.tmpl.html',
+            '﻿<div>\n' +
+            '    <div ng-transclude></div>\n' +
+            '    <pre ng-show="matches.length" ng-repeat="error in matches">{{error|json}}</pre>\n' +
+            '</div>');
+    }
+]);
+
+app.controller('MainCtrl', function($timeout) {
     var viewModel = this;
 
     // fake gettings errors back from an api in this format
-    viewModel.submit = function () {
+    viewModel.submit = function() {
         viewModel.action = 'Submitting...';
 
-        $timeout(function () {
+        $timeout(function() {
             viewModel.errors = [
-              { propertyName: '', type: 'length', message: 'This is global' },
-              { propertyName: '', type: 'length', message: 'This is global also' },
-              { propertyName: 'firstName', type: 'required', message: 'First Name is required' },
-              { propertyName: 'firstName', type: 'length', message: 'First Name must be between 2 and 256 characters' },
-              { propertyName: 'lastName', type: 'length', message: 'Last Name must be between 2 and 256 characters' }
+                { propertyName: '', type: 'length', message: 'This is global' },
+                { propertyName: '', type: 'length', message: 'This is global also' },
+                { propertyName: 'firstName', type: 'required', message: 'First Name is required' },
+                { propertyName: 'firstName', type: 'length', message: 'First Name must be between 2 and 256 characters' },
+                { propertyName: 'lastName', type: 'length', message: 'Last Name must be between 2 and 256 characters' }
             ];
 
             viewModel.action = 'Submitting';
@@ -22,6 +33,7 @@ app.controller('MainCtrl', function ($timeout) {
     };
 
     init();
+
     function init() {
         viewModel.action = 'Submitting';
     }
