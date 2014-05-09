4c54955200b49b3cf12d996b70b89a80b181beb0
Justin Obney <justinobney@Justins-MacBook-Air.local>
2014-05-09 09:16:16 -0500

    began developing oTable directive

---
+++ src/datatables/o-table.spec.js
@@ -0,0 +1,70 @@
+(function() {
+    'use strict';
+
+    describe('Directive: oTable', function() {
+
+        // load the service's module
+        beforeEach(module('envoc.directives.datatables'));
+
+        var $rootScope,
+            $compile,
+            $httpBackend,
+            element,
+            scope;
+
+        beforeEach(inject(function($injector) {
+            $rootScope = $injector.get('$rootScope');
+            $compile = $injector.get('$compile');
+            $httpBackend = $injector.get('$httpBackend');
+            scope = $rootScope.$new();
+        }));
+
+        it('should compile and requires config', function() {
+            element = angular.element('<div o-table config="config"><div o-table-default></div></div>');
+            expect(compile).toThrow();
+
+            scope.config = {
+                dataSrcUrl: '/data/get'
+            }
+
+            element = angular.element('<div o-table config="config"><div o-table-default></div></div>');
+            expect(compile).not.toThrow();
+
+            function compile() {
+                element = $compile(element)(scope);
+            }
+        });
+
+        describe('configuration', function() {
+            beforeEach(function() {
+                scope.config = {
+                    dataSrcUrl: '/data/get'
+                }
+            });
+
+            it('should post to config url', function() {
+                element = angular.element('<div o-table config="config" id="childScope"><div o-table-default>{{data.iTotalRecords}}</div></div>');
+                element = $compile(element)(scope);
+
+                $httpBackend
+                    .expect('POST', scope.config.dataSrcUrl)
+                    .respond(httpResponse1);
+
+                $httpBackend.flush();
+                $rootScope.$digest();
+                // expect(element.text()).toBe(2);
+            });
+        });
+    });
+
+    var httpResponse1 = {
+        "sEcho": 1,
+        "iTotalRecords": 2,
+        "iTotalDisplayRecords": 2,
+        "aaData": [
+            [2, "\/Date(1383171547680)\/", null, "No", 4],
+            [1, "\/Date(1383171098853)\/", "\/Date(1383171394617)\/", "Yes", 1]
+        ],
+        "sColumns": "Id,StartDateUtc,EndDateUtc,IsClosed,RegistrationCount"
+    }
+})();
