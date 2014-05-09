e73147c4d1bbf00d76c5d647d4be216652cdc130
justinobney <justin@envoc.com>
2014-05-09 13:56:18 -0500

    build updates, creating oTableDefault template

---
+++ build/partials/datatables/oTableDefault.tmpl.js
@@ -0,0 +1,66 @@
+(function(module) {
+try {
+  module = angular.module('envoc.directives.partials');
+} catch (e) {
+  module = angular.module('envoc.directives.partials', []);
+}
+module.run(['$templateCache', function($templateCache) {
+  $templateCache.put('/oTemplates/datatables/oTableDefault.tmpl.html',
+    '<div class="dataTables_wrapper form-inline" role="grid">\n' +
+    '    <div class="row">\n' +
+    '        <div class="span6 pull-left">\n' +
+    '            <div class="dataTables_length">\n' +
+    '                <label>Show\n' +
+    '                    <select size="1" aria-controls="user-table">\n' +
+    '                        <option value="10" selected="selected">10</option>\n' +
+    '                        <option value="25">25</option>\n' +
+    '                        <option value="50">50</option>\n' +
+    '                        <option value="100">100</option>\n' +
+    '                    </select>entries</label>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '        <div class="span6 pull-right">\n' +
+    '            <div class="dataTables_filter">\n' +
+    '                <label>Search:\n' +
+    '                    <input type="text" aria-controls="user-table">\n' +
+    '                </label>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '    <table class="table table-striped dataTable" aria-describedby="user-table_info">\n' +
+    '        <thead>\n' +
+    '            <tr role="row"></tr>\n' +
+    '        </thead>\n' +
+    '\n' +
+    '        <tbody role="alert" aria-live="polite" aria-relevant="all">\n' +
+    '            <tr ng-repeat="row in rows">\n' +
+    '                <td>\n' +
+    '                    {{row}}\n' +
+    '                </td>\n' +
+    '            </tr>\n' +
+    '        </tbody>\n' +
+    '    </table>\n' +
+    '    <div class="row">\n' +
+    '        <div class="span6 pull-left">\n' +
+    '            <div class="dataTables_info" id="user-table_info">Showing 1 to 4 of 4 entries</div>\n' +
+    '        </div>\n' +
+    '        <div class="span6 pull-right">\n' +
+    '            <div class="dataTables_paginate paging_bootstrap">\n' +
+    '                <ul class=" pagination">\n' +
+    '                    <li class="prev disabled">\n' +
+    '                        <a href="#">← Previous</a>\n' +
+    '                    </li>\n' +
+    '                    <li class="active">\n' +
+    '                        <a href="#">1</a>\n' +
+    '                    </li>\n' +
+    '                    <li class="next disabled">\n' +
+    '                        <a href="#">Next → </a>\n' +
+    '                    </li>\n' +
+    '                </ul>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '    </div>\n' +
+    '</div>\n' +
+    '');
+}]);
+})();
