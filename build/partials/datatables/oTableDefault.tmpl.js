1619d09b97be9ca6a34d18cd34588c95a0c0ce96
Justin Obney <justinobney@Justins-MacBook-Air.local>
2014-05-10 00:31:53 -0500

    TEMPLATE: altering oTableDefault

--- build/partials/datatables/oTableDefault.tmpl.js
+++ build/partials/datatables/oTableDefault.tmpl.js
@@ -6,14 +6,15 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('/oTemplates/datatables/oTableDefault.tmpl.html',
+    '<pre>{{ctrl.state|json}}</pre>\n' +
     '<div class="dataTables_wrapper form-inline" role="grid">\n' +
     '    <div class="row">\n' +
     '        <div class="span6 pull-left">\n' +
     '            <div class="dataTables_length">\n' +
     '                <label>\n' +
     '                    Show\n' +
-    '                    <select size="1" aria-controls="user-table">\n' +
-    '                        <option value="10" selected="selected">10</option>\n' +
+    '                    <select size="1" ng-model="ctrl.state.linesPerPage">\n' +
+    '                        <option value="10">10</option>\n' +
     '                        <option value="25">25</option>\n' +
     '                        <option value="50">50</option>\n' +
     '                        <option value="100">100</option>\n' +
@@ -26,41 +27,31 @@ module.run(['$templateCache', function($templateCache) {
     '            <div class="dataTables_filter">\n' +
     '                <label>\n' +
     '                    Search:\n' +
-    '                    <input type="text" aria-controls="user-table">\n' +
+    '                    <input type="text" ng-model="ctrl.state.allSearch">\n' +
     '                </label>\n' +
     '            </div>\n' +
     '        </div>\n' +
     '    </div>\n' +
     '\n' +
-    '    <table class="table table-striped dataTable" aria-describedby="user-table_info">\n' +
+    '    <table class="table table-striped dataTable">\n' +
     '        <thead>\n' +
     '            <tr role="row"></tr>\n' +
     '        </thead>\n' +
     '\n' +
     '        <tbody role="alert" aria-live="polite" aria-relevant="all">\n' +
-    '            <tr ng-repeat="row in rows">\n' +
+    '            <tr ng-repeat="row in ctrl.data">\n' +
     '            </tr>\n' +
     '        </tbody>\n' +
     '    </table>\n' +
     '\n' +
     '    <div class="row">\n' +
     '        <div class="span6 pull-left">\n' +
-    '            <div class="dataTables_info" id="user-table_info">Showing 1 to 4 of 4 entries</div>\n' +
+    '            <div class="dataTables_info" id="user-table_info">Showing {{ctrl.state.startIdx + 1}} to {{ctrl.state.startIdx + ctrl.state.linesPerPage}} of {{ctrl.state.iTotalRecords}} entries</div>\n' +
     '        </div>\n' +
     '\n' +
     '        <div class="span6 pull-right">\n' +
     '            <div class="dataTables_paginate paging_bootstrap">\n' +
-    '                <ul class=" pagination">\n' +
-    '                    <li class="prev disabled">\n' +
-    '                        <a href="#">← Previous</a>\n' +
-    '                    </li>\n' +
-    '                    <li class="active">\n' +
-    '                        <a href="#">1</a>\n' +
-    '                    </li>\n' +
-    '                    <li class="next disabled">\n' +
-    '                        <a href="#">Next → </a>\n' +
-    '                    </li>\n' +
-    '                </ul>\n' +
+    '                <pagination total-items="ctrl.state.iTotalDisplayRecords" ng-model="ctrl.state.currentPage"></pagination>\n' +
     '            </div>\n' +
     '        </div>\n' +
     '    </div>\n' +
