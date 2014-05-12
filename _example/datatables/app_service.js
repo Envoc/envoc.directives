ad4c1738c19c148db2c3677a2e10d0927825db9a
Justin Obney <justinobney@Justins-MacBook-Air.local>
2014-05-12 07:04:27 -0500

    adding another table example

--- _example/datatables/app_service.js
+++ _example/datatables/app_service.js
@@ -8,10 +8,16 @@ app.controller('MainCtrl', function(FakeService) {
         fetchMethod: FakeService.fetch
     };
 
+    viewModel.config2 = {
+        dataSrc: []
+    };
+
     init();
 
     function init() {
-        
+        _.times(33, function(n){
+            viewModel.config2.dataSrc.push({id:n+1, name:'User '+ n, position: 'Developer Level ' + (n+1)});
+        })
     }
 });
 
@@ -19,8 +25,8 @@ app.service('FakeService', function($q) {
     var self = this;
 
     var httpResponse1 = {
-        "iTotalRecords": 2,
-        "iTotalDisplayRecords": 2,
+        "iTotalRecords": 4,
+        "iTotalDisplayRecords": 4,
         "aaData": [
             [4, "\/Date(1383171547680)\/", null, "No", 4],
             [3, "\/Date(1383171098853)\/", "\/Date(1383171394617)\/", "Yes", 1],
