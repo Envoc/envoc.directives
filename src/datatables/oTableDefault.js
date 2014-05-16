bc66bd2a1b314b571353c3887c3860ca32fa5fdd
justinobney <justin@envoc.com>
2014-05-16 09:11:54 -0500

    forced to make the default a little more declarative

--- src/datatables/oTableDefault.js
+++ src/datatables/oTableDefault.js
@@ -8,47 +8,15 @@
                 templateUrl: '/oTemplates/datatables/oTableDefault.tmpl.html',
                 require: '^oTable',
                 restrict: 'EA',
+                transclude: true,
                 scope: {},
                 compile: function compile(tElement, tAttrs, transclude) {
-                    if(!tAttrs.fields) {
-                        throw new Error('No field list included');
-                    }
-
-                    var theBodyRow = tElement.find('tbody').find('tr');
-                    var theHeadRow = tElement.find('thead').find('tr');
-
-                    var theFields = tAttrs.fields.split(',');
-                    
-                    var sortable = (tAttrs.sortable || '').split(',');
-
-                    var headerHtml = '';
-                    angular.forEach(theFields, function(val){
-                        var sortTemplate = '<span class="pull-right"><span class="asc">asc</span><span class="desc">desc</span></span>'
-                        var found = sortable.indexOf(val) > -1;
-                        var header = found ? sortTemplate : '';
-                        var headerAttr = found ? 'o-table-sort field="' + val + '"' : '';
-                        var html =  '<th ' + headerAttr + '>' + 
-                                        val +
-                                        header +
-                                    '</th>'
-                        headerHtml = headerHtml + html;
-                    });
-
-                    theHeadRow.html(headerHtml)
-
-                    buildTemplate(theBodyRow, '<td>{{row.', '}}</td>', theFields);
-
-                    function buildTemplate(theRow, startTemplate, endTemplate, fields) {
-                        var fieldTemplate = startTemplate + fields.join(endTemplate + startTemplate) + endTemplate;
-
-                        theRow.html(fieldTemplate);
-                    }
-
                     return function postLink(scope, iElement, iAttrs, controller) {
                         ctor();
                         function ctor() {
                             scope.ctrl = controller;
                             scope.$watch(getOTableData, applyToScope);
+                            iElement.find('table').addClass('table table-striped dataTable');
                         }
 
                         function getOTableData() {
