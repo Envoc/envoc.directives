90701afcd77e18bc28707992427513c0a8ebbc46
Robert Schroeder <rschroeder@envoc.com>
2014-06-10 10:00:41 -0500

    added validation summary directive

---
+++ src/validation/oValidationSummary.js
@@ -0,0 +1,25 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('envoc.directives.validation')
+        .directive('oValidationSummary', [
+            function() {
+                return {
+                    restrict: 'EA',
+                    require: '^oValidateWith',
+                    templateUrl: '/oTemplates/validation/oValidationSummary.tmpl.html',
+                    transclude: true,
+                    scope: true,
+                    link: function(scope, element, attr, oValidateWithCtrl) {
+
+                        scope.$watch(getErrors, function(current, previous) {
+                            scope.errors = current;
+                        });
+
+                        function getErrors() { return oValidateWithCtrl.errors; }
+                    }
+                };
+            }
+        ]);
+})();
