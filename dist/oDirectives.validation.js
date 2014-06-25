2ec2ac02d0ad21d761a43e4e7986470df35aa59e
justinobney <justin@envoc.com>
2014-06-25 14:24:00 -0500

    chore: build

--- dist/oDirectives.validation.js
+++ dist/oDirectives.validation.js
@@ -105,12 +105,21 @@ module.run(['$templateCache', function($templateCache) {
                     scope: true,
                     link: function(scope, element, attr, oValidateWithCtrl) {
                         var key = attr.oValidationMessageFor.toLowerCase();
+                        var altMessageName = attr.altMessageName;
+                        var propertyName = attr.altPropertyName || 'propertyName';
 
                         scope.$watch(getErrors, function(current, previous) {
                             if (current) {
                                 scope.matches = current.filter(function(error) {
-                                    return error.propertyName.toLowerCase() == key;
+                                    return angular.isDefined(error[propertyName]) && error[propertyName].toLowerCase() == key;
                                 });
+
+                                if(altMessageName){
+                                    scope.matches = scope.matches.map(function(obj, idx){
+                                        obj[altMessageName] && (obj.errorMessage = obj[altMessageName]);
+                                        return obj;
+                                    });
+                                }
                             }
                         });
 
