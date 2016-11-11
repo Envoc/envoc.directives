d1ff2b182973748d21b0199de7d0efa8d61f363c
Robert Schroeder <rschroeder@envoc.com>
2016-11-11 14:32:20 -0600

    fix: updates default datatable template to bootstrap 3

--- dist/oDirectives.js
+++ dist/oDirectives.js
@@ -1,5 +1,5 @@
 /*
- * envoc.directives 0.12.0
+ * envoc.directives 0.12.1
  * Author: Envoc
  * Repository: https://github.com/Envoc/envoc.directives
  */
@@ -239,13 +239,13 @@ module.run(['$templateCache', function($templateCache) {
   $templateCache.put('/oTemplates/datatables/oTableDefault.tmpl.html',
     '<div class="dataTables_wrapper form-inline" role="grid" ng-cloak>\n' +
     '    <div class="row">\n' +
-    '        <div class="span6 pull-left">\n' +
+    '        <div class="col-sm-6">\n' +
     '            <div class="dataTables_length">\n' +
     '                <div o-table-lines-per-page></div>\n' +
     '            </div>\n' +
     '        </div>\n' +
-    '        <div class="span6 pull-right">\n' +
-    '            <div class="dataTables_filter">\n' +
+    '        <div class="col-sm-6">\n' +
+    '            <div class="dataTables_filter pull-right">\n' +
     '                <label>\n' +
     '                    {{ctrl.lang.search}}:\n' +
     '                    <input type="text" o-table-filter>\n' +
@@ -253,7 +253,9 @@ module.run(['$templateCache', function($templateCache) {
     '            </div>\n' +
     '        </div>\n' +
     '    </div>\n' +
-    '    \n' +
+    '\n' +
+    '    <div class="row"><div class="col-sm-12"><div o-table-page-info></div></div></div>\n' +
+    '\n' +
     '    <div class="alert alert-info" ng-show="ctrl.loading">\n' +
     '        <strong>{{ctrl.lang.loading}}</strong>\n' +
     '    </div>\n' +
@@ -265,18 +267,16 @@ module.run(['$templateCache', function($templateCache) {
     '    <div ng-transclude></div>\n' +
     '\n' +
     '    <div class="row" ng-show="ctrl.data.length">\n' +
-    '        <div class="span6 pull-left">\n' +
+    '        <div class="col-sm-6">\n' +
     '            <div o-table-page-info></div>\n' +
     '        </div>\n' +
-    '\n' +
-    '        <div class="span6 pull-right">\n' +
-    '            <div class="dataTables_paginate paging_bootstrap">\n' +
+    '        <div class="col-sm-6">\n' +
+    '            <div class="dataTables_paginate paging_bootstrap pull-right">\n' +
     '                <div o-table-pagination></div>\n' +
     '            </div>\n' +
     '        </div>\n' +
     '    </div>\n' +
-    '</div>\n' +
-    '');
+    '</div>');
 }]);
 })();
 
